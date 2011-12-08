a1837aff023a59c61a1d264fe006f616f738bda8
Andrew Leaver-Fay <aleaverfay@gmail.com>
2011-12-08 23:07:50 +0000

    Removing src.settings files that should have been deleted after my library rearrangement earlier this afternoon.  Thanks, James, for the heads up.  Adding .dot file output that can be fed into graphviz to automatically plot the inter-library dependencies in protocols
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta/rosetta_tools@46365 6189c7af-7406-0410-a2ce-f22f51069c23

--- python_cc_reader/determine_protocols_library_dependencies.py
+++ python_cc_reader/determine_protocols_library_dependencies.py
@@ -16,6 +16,10 @@ def library_level_and_column_from_src_settingsfile_name( libprefix, lib_srcsetti
 
 if __name__ == "__main__" :
 
+   graphviz_output = False;
+   if len( sys.argv ) > 1 and sys.argv[1] == "--graphviz" :
+      graphviz_output = True
+
    #inclusion_graph.remove_known_circular_dependencies_from_graph( g )
    #tg = inclusion_graph.transitive_closure( g )
 
@@ -43,9 +47,9 @@ if __name__ == "__main__" :
    #sys.exit(0)
 
    compilable_files, all_includes, file_contents = code_utilities.load_source_tree()
-   print "...source tree loaded"
+   #print "...source tree loaded"
    g = inclusion_graph.create_graph_from_includes( all_includes )
-   print "...inclusion graph created"
+   #print "...inclusion graph created"
 
    for edge in g.edges() :
       file1,file2 = edge
@@ -57,13 +61,31 @@ if __name__ == "__main__" :
       node1 = str(lev1) + col1
       node2 = str(lev2) + col2
       if node2 not in protocols_library_graph.neighbors( node1 ) :
-          print "adding edge", node1, node2
+          #print "adding edge", node1, node2
           protocols_library_graph.add_edge( node1, node2 )
 
-   protocols_tg = inclusion_graph.transitive_closure( protocols_library_graph )
-   for node in protocols_tg.nodes() :
-      print "Dependencies for node", node
-      for node_neighbor in protocols_tg.neighbors( node ) :
-          print "  ", node_neighbor
-
+   #protocols_tg = inclusion_graph.transitive_closure( protocols_library_graph )
+   if not graphviz_output :
+      protocols_tg = protocols_library_graph
+      for node in protocols_tg.nodes() :
+         print "Dependencies for node", node
+         for node_neighbor in protocols_tg.neighbors( node ) :
+            print "  ", node_neighbor
+   else :
+      print "digraph G {"
+      for level in xrange(len(prot_levels)) :
+         print "subgraph level" + str(level+1) + " { rank = same; ",
+         if len(prot_levels[level]) == 1 :
+            print "p" + str(level+1) + ";}"
+         else:
+            for column in xrange(len(prot_levels[level])) :
+               print "p" + str(level+1) + chr( ord('a') + column ) + ";",
+            print "}"
+      for node in protocols_library_graph.nodes() :
+         print "p" + node + ";"
+      for node in protocols_library_graph.nodes() :
+         for node2 in protocols_library_graph.neighbors( node ) :
+            if node == node2 : continue
+            print "p"+ node2, "-> p" + node + " [ dir=back ];"
+      print "}"
 

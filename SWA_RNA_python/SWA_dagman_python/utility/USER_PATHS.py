ba2897a0a9ab41035d89acea24e3e230c7eadea8
Parin Sripakdeevong <sripakpa@stanford.edu>
2012-03-18 13:55:26 +0000

    Relocate files in rosetta_tools/SWA_RNA_python/. Add rosetta_demos/public/SWA_RNA_Loop/example_output/
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta/rosetta_tools@47978 6189c7af-7406-0410-a2ce-f22f51069c23

--- SWA_RNA_python/SWA_dagman_python/utility/USER_PATHS.py
+++ SWA_RNA_python/SWA_dagman_python/utility/USER_PATHS.py
@@ -2,6 +2,3 @@
 
 USER_ROSETTA_BIN_FOLDER="~/ROSETTA/rosetta_UPDATE_V3/rosetta_source/bin/"    
 USER_ROSETTA_DATABASE_FOLDER="~/ROSETTA/rosetta_UPDATE_V3/rosetta_database/" 
-
-#USER_ROSETTA_BIN_FOLDER="~/ROSETTA/rosetta_UPDATE_V2/rosetta_source/bin/"    
-#USER_ROSETTA_DATABASE_FOLDER="~/ROSETTA/rosetta_UPDATE_V2/rosetta_database/" 

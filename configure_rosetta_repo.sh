63d9c97ebe8cea5499771645e5c6af3b95219df5
Brian Weitzner <brian.weitzner@gmail.com>
2013-04-10 15:27:53 -0400

    Adding aliases and colors to the configuration script.
    The structure of the script is no longer a while loop with a break
    at the end, which is, duh, better.  I think this now does all of
    the things we want it to do.
    
    A modification I can forsee is that we may be moving the git_hooks
    directory and that will need to be reflected here.

--- configure_rosetta_repo.sh
+++ configure_rosetta_repo.sh
@@ -21,59 +21,70 @@ fi
 
 github_user_name=$1
 repo="rosetta"
-while true; do
-    read -p "Where do you to clone $repo? " path
-    if [ ! -d $path ]
-    then
-        echo "\033[0;33m'$path' does not exist!\033[0m You'll need to create '$path' if you want to install rosetta there."
-        while true; do
-            read -p "Would you like to create this directory now? " yn
-            case $yn in
-                [Yy]* ) mkdir $path; break;;
-                [Nn]* ) exit;;
-                * ) echo "Please answer yes or no.";;
-            esac
-        done
-    fi  
-    
-    echo "\033[0;34mCloning Rosetta...\033[0m"
-    hash git >/dev/null && /usr/bin/env git clone git@github.com:RosettaCommons/$repo.git $path/$repo || {
-    echo "git is not installed!"
-    exit
-    }
-                                    
-    echo "\033[0;32m"'     ___           ___           ___           ___           __         ___         ___      '"\033[0m"
-    echo "\033[0;32m"'    /\  \         /\  \         /\__\         /\__\         /\__\      /\__\       /\  \     '"\033[0m"
-    echo "\033[0;32m"'   /::\  \       /::\  \       /:/ _/_       /:/ _/_       /:/  /     /:/  /      /::\  \    '"\033[0m"
-    echo "\033[0;32m"'  /:/\:\__\     /:/\:\  \     /:/ /\  \     /:/ /\__\     /:/__/     /:/__/      /:/\:\  \   '"\033[0m"
-    echo "\033[0;32m"' /:/ /:/  /    /:/  \:\  \   /:/ /::\  \   /:/ /:/ _/_   /::\  \    /::\  \     /:/ /::\  \  '"\033[0m"
-    echo "\033[0;32m"'/:/_/:/__/___ /:/__/ \:\__\ /:/_/:/\:\__\ /:/_/:/ /\__\ /:/\:\  \  /:/\:\  \   /:/_/:/\:\__\ '"\033[0m"
-    echo "\033[0;32m"'\:\/:::::/  / \:\  \ /:/  / \:\/:/ /:/  / \:\/:/ /:/  / \/__\:\  \ \/__\:\  \  \:\/:/  \/__/ '"\033[0m"
-    echo "\033[0;32m"' \::/~~/~~~~   \:\  /:/  /   \::/ /:/  /   \::/_/:/  /       \:\  \     \:\  \  \::/__/      '"\033[0m"
-    echo "\033[0;32m"'  \:\~~\        \:\/:/  /     \/_/:/  /     \:\/:/  /         \:\  \     \:\  \  \:\  \      '"\033[0m"
-    echo "\033[0;32m"'   \:\__\        \::/  /        /:/  /       \::/  /           \:\__\     \:\__\  \:\__\     '"\033[0m"
-    echo "\033[0;32m"'    \/__/         \/__/         \/__/         \/__/             \/__/      \/__/   \/__/     '"\033[0m"
 
-    echo "\n\n \033[0;32m....is now cloned.\033[0m"
-	
-	starting_dir=pwd
-	cd $path/$repo
-	
-	echo "\033[0;34mDisabling fast-forward merges on master...\033[0m"
-	git config branch.master.mergeoptions "--no-ff"
-	
-	echo "\033[0;34mConfiguring commit message template...\033[0m"
-	git config commit.template .commit_template.txt
-	
-	cd .git/hooks
-	url="https://github.com/RosettaCommons/rosetta_tools/raw/master"
-	for hook in pre-commit post-commit; do 
-		echo "\033[0;34mConfiguring the $hook hook...\033[0m"
-		curl -u $github_user_name -L $url/git_hooks/$hook > $hook
-		chmod +x $hook
+read -p "Where do you to clone $repo? " path
+if [ ! -d $path ]
+then
+	echo "\033[0;33m'$path' does not exist!\033[0m You'll need to create '$path' if you want to install rosetta there."
+	while true; do
+		read -p "Would you like to create this directory now? " yn
+		case $yn in
+			[Yy]* ) mkdir $path; break;;
+			[Nn]* ) exit;;
+			* ) echo "Please answer yes or no.";;
+		esac
 	done
-	
-	cd $starting_dir
-	echo "\033[0;32mDone configuring your Rosetta git repository!\033[0m"
-    break;
+fi  
+
+echo "\033[0;34mCloning Rosetta...\033[0m"
+hash git >/dev/null && /usr/bin/env git clone git@github.com:RosettaCommons/$repo.git $path/$repo || {
+echo "git is not installed!"
+exit
+}
+								
+echo "\033[0;32m"'     ___           ___           ___           ___           __         ___         ___      '"\033[0m"
+echo "\033[0;32m"'    /\  \         /\  \         /\__\         /\__\         /\__\      /\__\       /\  \     '"\033[0m"
+echo "\033[0;32m"'   /::\  \       /::\  \       /:/ _/_       /:/ _/_       /:/  /     /:/  /      /::\  \    '"\033[0m"
+echo "\033[0;32m"'  /:/\:\__\     /:/\:\  \     /:/ /\  \     /:/ /\__\     /:/__/     /:/__/      /:/\:\  \   '"\033[0m"
+echo "\033[0;32m"' /:/ /:/  /    /:/  \:\  \   /:/ /::\  \   /:/ /:/ _/_   /::\  \    /::\  \     /:/ /::\  \  '"\033[0m"
+echo "\033[0;32m"'/:/_/:/__/___ /:/__/ \:\__\ /:/_/:/\:\__\ /:/_/:/ /\__\ /:/\:\  \  /:/\:\  \   /:/_/:/\:\__\ '"\033[0m"
+echo "\033[0;32m"'\:\/:::::/  / \:\  \ /:/  / \:\/:/ /:/  / \:\/:/ /:/  / \/__\:\  \ \/__\:\  \  \:\/:/  \/__/ '"\033[0m"
+echo "\033[0;32m"' \::/~~/~~~~   \:\  /:/  /   \::/ /:/  /   \::/_/:/  /       \:\  \     \:\  \  \::/__/      '"\033[0m"
+echo "\033[0;32m"'  \:\~~\        \:\/:/  /     \/_/:/  /     \:\/:/  /         \:\  \     \:\  \  \:\  \      '"\033[0m"
+echo "\033[0;32m"'   \:\__\        \::/  /        /:/  /       \::/  /           \:\__\     \:\__\  \:\__\     '"\033[0m"
+echo "\033[0;32m"'    \/__/         \/__/         \/__/         \/__/             \/__/      \/__/   \/__/     '"\033[0m"
+
+echo "\n\n \033[0;32m....is now cloned.\033[0m"
+
+starting_dir=pwd
+cd $path/$repo
+
+echo "\033[0;34mDisabling fast-forward merges on master...\033[0m"
+git config branch.master.mergeoptions "--no-ff"
+
+echo "\033[0;34mConfiguring commit message template...\033[0m"
+git config commit.template .commit_template.txt
+
+cd .git/hooks
+url="https://github.com/RosettaCommons/rosetta_tools/raw/master"
+for hook in pre-commit post-commit; do 
+	echo "\033[0;34mConfiguring the $hook hook...\033[0m"
+	curl -u $github_user_name -L $url/git_hooks/$hook > $hook
+	chmod +x $hook
 done
+
+
+echo "\033[0;34mConfiguring aliases...\033[0m"
+git config alias.tracked-branch "\!sh -c 'git checkout -b \$1 && git push origin \$1:\$2/\$1 && git branch --set-upstream \$1  origin/\$2/\$1' -"
+git config alias.personal-tracked-branch "\!sh -c 'git tracked-branch \$1 $github_user_name' -"
+
+git config --global alias.show-graph "log --graph --abbrev-commit --pretty=oneline"
+
+echo "\033[0;34mConfiguring git colors...\033[0m"
+git config --global color.branch auto
+git config --global color.diff auto
+git config --global color.interactive auto
+git config --global color.status auto
+	
+cd $starting_dir
+echo "\033[0;32mDone configuring your Rosetta git repository!\033[0m"

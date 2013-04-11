c1baf9a094ccda0b16659ec28c2151fb0308a32c
Fang-Chieh Chou <fcchou@stanford.edu>
2013-04-11 03:19:54 +0000

    Add rna_helix.py for esay call of rna_helix app
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta/rosetta_tools@54772 6189c7af-7406-0410-a2ce-f22f51069c23

--- rna_tools/pdb_util/renumber_pdb_in_place.py
+++ rna_tools/pdb_util/renumber_pdb_in_place.py
@@ -1,63 +1,65 @@
 #!/usr/bin/python
 
-import string
-from sys import argv,stderr,stdout
-from os import popen,system
-from os.path import exists
-from amino_acids import longer_names
+from sys import argv
+from os import system
 from parse_options import parse_options, get_ints
 
-retain_atom_num = parse_options( argv, "retain_atom_num", 0 )
 
-assert( len(argv)>1)
+def renumber_pdb(pdbnames, new_numbers, retain_atom_num=0):
+    for pdbname in pdbnames:
+        lines = open(pdbname,'r').readlines()
 
-# Look for integers... perhaps we are specifying particular residue numbers
-pdbnames = []
-new_numbers = []
-for i in range(1, len( argv ) ):
-	if not get_ints( argv[i], new_numbers ):
-		pdbnames.append( argv[i] )
+        oldresnum = '   '
+        count = 0;
 
-for pdbname in pdbnames:
-	lines = open(pdbname,'r').readlines()
+        outid  = open( 'temp.txt','w')
 
-	oldresnum = '   '
-	count = 0;
+        atomnum  = 0
+        for line in lines:
+            line_edit = line
+            if line[0:3] == 'TER':
+                continue
 
-	outid  = open( 'temp.txt','w')
+            if line_edit[0:4] == 'ATOM' or line_edit[0:6] == 'HETATM':
 
-	atomnum  = 0
-	for line in lines:
-		line_edit = line
-		if line[0:3] == 'TER':
-		    continue
+                if not (line[16]==' ' or line[16]=='A'): continue
 
-		if line_edit[0:4] == 'ATOM' or line_edit[0:6] == 'HETATM':
+                atomnum += 1
 
-		    if not (line[16]==' ' or line[16]=='A'): continue
+                resnum = line_edit[23:26]
+                if not resnum == oldresnum:
+                    count = count + 1
+                    oldresnum = resnum
 
-		    atomnum += 1
+                if ( count <= len( new_numbers ) ):
+                    newnum = '%4d' % new_numbers[ count-1 ]
+                else:
+                    if len( new_numbers) > 0: print 'WARNING! residue number %d is greater than length of input numbering %d' % (count, len( new_numbers) )
+                    newnum = '%4d' % count
 
-		    resnum = line_edit[23:26]
-		    if not resnum == oldresnum:
-			count = count + 1
-		    oldresnum = resnum
+                if retain_atom_num:
+                    line_edit = '%s%s%s' % (line_edit[0:22], newnum, line_edit[26:] )
+                else:
+                    line_edit = '%s%5d%s%s%s' % (line_edit[0:6],atomnum,line[11:22], newnum, line_edit[26:] )
 
-		    if ( count <= len( new_numbers ) ):
-			    newnum = '%4d' % new_numbers[ count-1 ]
-		    else:
-			    if len( new_numbers) > 0: print 'WARNING! residue number %d is greater than length of input numbering %d' % (count, len( new_numbers) )
-			    newnum = '%4d' % count
+                outid.write(line_edit)
 
-		    if retain_atom_num:
-			    line_edit = '%s%s%s' % (line_edit[0:22], newnum, line_edit[26:] )
-		    else:
-			    line_edit = '%s%5d%s%s%s' % (line_edit[0:6],atomnum,line[11:22], newnum, line_edit[26:] )
+        if ( count < len( new_numbers) ): print 'WARNING! number of residues %d is less than length of input numbering %d' % (count, len( new_numbers) )
 
-		    outid.write(line_edit)
+        outid.close()
 
-	if ( count < len( new_numbers) ): print 'WARNING! number of residues %d is less than length of input numbering %d' % (count, len( new_numbers) )
+        system( 'mv temp.txt '+pdbname )
 
-	outid.close()
+if __name__ == '__main__':
+    retain_atom_num = parse_options( argv, "retain_atom_num", 0 )
 
-	system( 'mv temp.txt '+pdbname )
+    assert( len(argv)>1)
+
+    # Look for integers... perhaps we are specifying particular residue numbers
+    pdbnames = []
+    new_numbers = []
+    for i in range(1, len( argv ) ):
+        if not get_ints( argv[i], new_numbers ):
+            pdbnames.append( argv[i] )
+
+    renumber_pdb(pdbnames, new_numbers, retain_atom_num)
---
+++ rna_tools/pdb_util/rna_helix.py
@@ -0,0 +1,40 @@
+import argparse
+import tempfile
+import subprocess
+import os
+from renumber_pdb_in_place import renumber_pdb
+from parse_options import get_ints
+
+parser = argparse.ArgumentParser(description='Run rna_helix to build a helix')
+parser.add_argument('-seq', required=True, help='Sequence of the helix, in the format: ggaa uucc', nargs=2 )
+parser.add_argument('-resnum', nargs=2, help='Renumber the residues with inputformat: 13-16 30-33')
+parser.add_argument('-o', default='helix.pdb', help='Filename of output pdb')
+args = parser.parse_args()
+
+print args
+
+
+#Build the helix
+temp = tempfile.NamedTemporaryFile(delete=False)
+cmdline = ( 'rna_helix  -rna::corrected_geo  -score::weights rna/rna_helix ' +
+            '-score:rna_torsion_potential RNA11_based_new ' +
+            '-geom_sol_correct_acceptor_base -chemical::enlarge_H_lj ')
+cmdline += '-o %s ' % temp.name
+cmdline += '-seq '
+for i in args.seq:
+    cmdline += i + ' '
+print cmdline
+subprocess.check_call(cmdline.split())
+
+#Renumber the pdb
+if args.resnum is None:
+    temp.close()
+    os.rename(temp.name, args.o)
+else: #renumbering
+    new_numbers = []
+    for i in args.resnum:
+        get_ints(i, new_numbers)
+    renumber_pdb([temp.name], new_numbers)
+    temp.close()
+    os.rename(temp.name, args.o)
+

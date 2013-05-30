ccffdb3c1fd5566283923f24de3e125d207eea9a
Jeffrey J. Gray <jgray@jhu.edu>
2013-05-30 17:15:59 -0500

    PyRosetta file that calculates kink geometry q and qbase

---
+++ antibody/kink_geom.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+
+from rosetta import *
+init()
+
+from rosetta.protocols.antibody import *
+import math
+import sys
+#from numpy import matrix
+
+
+def kink_begin(abinfo):
+    H3_end = abinfo.get_CDR_loop(h3).stop()
+    return H3_end - 1
+
+def kink_end(abinfo):
+    H3_end = abinfo.get_CDR_loop(h3).stop()
+    return H3_end + 2
+
+
+
+def kink_geom(pose):
+    poseAI = AntibodyInfo(pose)
+    print poseAI
+
+
+def dihedral(p1,p2,p3,p4):
+    a = p2-p1
+    b = p3-p2
+    c = p4-p3
+    a.normalize()
+    b.normalize()
+    c.normalize()
+    x = -a.dot(c) + a.dot(b) * b.dot(c)
+    y = a.dot( b.cross(c) )
+    angle = math.atan2(y,x)  # angle in [ -pi, pi ]  # degenerate if x or y == 0?
+    angle = angle * 180 / math.pi
+    return angle
+
+
+def antibody_kink_geometry(filename):
+
+    pose = pose_from_pdb(filename)
+    abinfo = AntibodyInfo(pose)
+    print abinfo
+
+    kb=kink_begin(abinfo)
+    ke=kink_end(abinfo)
+
+    kr0=pose.residue(kb)
+    kr1=pose.residue(kb+1)
+    kr2=pose.residue(kb+2)
+    kr3=pose.residue(kb+3)
+    kseq = kr0.name1() + kr1.name1() + kr2.name1() + kr3.name1()
+    print "Kink is defined from pose residues %i-%i: %s" % (kb,ke,kseq)
+
+    CA0=kr0.xyz("CA")
+    CA1=kr1.xyz("CA")
+    CA2=kr2.xyz("CA")
+    CA3=kr3.xyz("CA")
+
+    q = (CA0 - CA3).magnitude
+    qbase = dihedral(CA0,CA1,CA2,CA3)
+    print "q = %f, qbase = %f degrees" % (q,qbase)
+    return (q,qbase)
+
+def main(args):
+    '''Calculate kink geometry for a set of antibodies.  Give filenames as arguments
+    '''
+
+    outf = file('kink_geom.dat', 'w')
+    outf.write("file\tq\tqbase\n")
+    for f in args[1:]:
+        print f
+        q = antibody_kink_geometry(f)
+        outf.write("%s\t%f\t%f\n" % (f,q[0],q[1]) )
+
+
+if __name__ == "__main__": main(sys.argv)
+

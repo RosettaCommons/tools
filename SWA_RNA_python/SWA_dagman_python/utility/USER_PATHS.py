67f2b3915d7a1e70d9688711a5706ad18aeecf7a
sripakpa <sripakpa@stanford.edu>
2013-06-11 17:31:18 -0700

    revert SWA_RNA_python/SWA_dagman_python/utility/USER_PATHS.py

--- SWA_RNA_python/SWA_dagman_python/utility/USER_PATHS.py
+++ SWA_RNA_python/SWA_dagman_python/utility/USER_PATHS.py
@@ -2,8 +2,7 @@
 
 import os
 
-#rosetta_folder = os.environ["ROSETTA"]
-rosetta_folder = "/Users/sripakpa/ROSETTA/git/Rosetta/"
+rosetta_folder = os.environ["ROSETTA"]
 
 USER_ROSETTA_BIN_FOLDER=rosetta_folder+"/main/source/bin/"
 USER_ROSETTA_DATABASE_FOLDER=rosetta_folder+"/main/database/"

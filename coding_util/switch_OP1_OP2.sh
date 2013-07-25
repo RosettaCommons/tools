9c848e211679dc062e5b3420eadbe1bc11101977
Rhiju Das <rhiju@stanford.edu>
2013-07-25 16:19:30 -0700

    small update to code renaming tools, in preparation for merge to master

--- coding_util/switch_OP1_OP2.sh
+++ coding_util/switch_OP1_OP2.sh
@@ -1,8 +1,8 @@
-#for i in cc hh params txt; do 
-#for i in params; do 
+# if you are on a mac, change "-i" to "-i .old" below.
+for i in "$@"; do 
  sed -i 's/OP1/OPX/g' $1
  sed -i 's/OP2/OP1/g' $1
  sed -i 's/OPX/OP2/g' $1
-#done
+done
 
-#find ./ -name '*.old' | xargs rm
+

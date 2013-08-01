8a6f5fb987c1b286df438a9a0b1fe2b204ec0f62
rhiju <rhiju@stanford.edu>
2013-08-01 09:52:06 -0700

    minor updates to allow scientific notation and operators *= and /=

--- coding_util/artistic_spacing.py
+++ coding_util/artistic_spacing.py
@@ -8,6 +8,12 @@ import argparse
 #args = parser.parse_args()
 #FORCE = args.force
 
+def Help():
+    print argv[ 0 ], ' [ --force ] file1 file2 ...'
+    exit( 0 )
+
+if len( argv ) <= 1: Help()
+
 FORCE = False
 if '--force' in argv:
     del( argv[ argv.index( '--force' ) ] )
@@ -19,13 +25,14 @@ operators1_to_space_before  = [ '+', '-', '=', ')' ]
 operators1_to_space_after   = [ '+', '-', '=', '(', ',', ';','}','{','<','>']
 operators1_to_space_before  = [ '+', '-', '=', ')', '<','>' ]
 
-operators2 = [ '==','++','--','+=','-=','!=','<=','>=','()','->','){','};','{}','<<','>>']
+operators2 = [ '*=', '/=', '==','++','--','+=','-=','!=','<=','>=','()','->','){','};','{}','<<','>>']
 
 operators2_to_space = [ '==','+=','-=','!=','<=','>=']
 operators2_do_not_space = [ '--', '++','->' ]
 
 ok_spaces = [ ' ',  '\t', '\n' ]
 paren_headers = [ 'if','while','for' ]
+numchars = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
 
 headers_for_less_than_symbol = [ 'map','vector1','vector','iterator','ptr','Matrix','Vector','cast' ]
 def check_headers_for_less_than_symbol( line, pos ):
@@ -65,9 +72,11 @@ for filename in argv[1:]:
                             if ( line[ pos - 1 ] not in ok_spaces ) and \
                                     line[ pos - 1 : pos + 1 ] not in operators2  and \
                                     line[ pos: pos + 2 ] not in operators2_do_not_space and \
-                                    ( sign != '<' or not check_headers_for_less_than_symbol( line, pos ) ):
-                                    new_line += ' '
-                                    space_added = True
+                                    ( sign != '<' or not check_headers_for_less_than_symbol( line, pos ) ) and \
+                                     ( sign != '-' or (line[ pos-1 ] != 'e') or (line[ pos-2 ] not in numchars ) ) : # 1e-3
+
+                                new_line += ' '
+                                space_added = True
                     if not space_added:
                         for sign in operators2_to_space:
                             if line[ pos : pos + 2] == sign :
@@ -82,6 +91,7 @@ for filename in argv[1:]:
             new_line += line[ pos ]
 
 
+
             if pos <= max_pos:
                 if pos < len( line )-1:
                     space_added = False
@@ -90,10 +100,11 @@ for filename in argv[1:]:
                             if ( line[ pos + 1 ] not in ok_spaces ) and \
                                     line[ pos : pos + 2 ] not in operators2 and \
                                     line[ pos - 1: pos + 1 ] not in operators2_do_not_space and \
-                                    ( sign != '-' or
-                                      ( line[ pos-1 ] not in ok_spaces ) ):
-                                new_line += ' '
-                                space_added = True
+                                    ( sign != '-' or \
+                                          ( ( line[ pos-1 ] not in ok_spaces ) and # ( -5 )
+                                            ( line[ pos -1 ] not in ['e','E'] or line[ pos - 2] not in numchars ) ) ):
+                                    new_line += ' '
+                                    space_added = True
                     if not space_added:
                         for sign in operators2_to_space:
                             if line[ pos - 1 : pos + 1 ] == sign :
@@ -125,3 +136,6 @@ for filename in argv[1:]:
 if not FORCE:
     print
     print "Changes *not* applied. Rerun with --force flag to apply changes."
+else:
+    print
+    print "Applied changes to files."

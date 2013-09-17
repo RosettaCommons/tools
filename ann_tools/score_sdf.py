5f85d03967ed44c095b4f272e8ef4f84ba47555b
Sam DeLuca <sam@decarboxy.com>
2013-09-17 08:51:58 -0500

    various changes to ann_tools/score_sdf.py to try to make it more efficient

--- ann_tools/score_sdf.py
+++ ann_tools/score_sdf.py
@@ -9,10 +9,11 @@ import os
 import sys
 import glob
 from optparse import OptionParser
-from random import shuffle
+from multiprocessing import Pool
 
-def score_sdf(bcl_path,input_filename,model_dir,output_table,nprocs,additional_score_list):
-    
+
+def score_sdf(job):
+    bcl_path,input_filenames,model_dir,output_path,additional_score_list = job
     additional_score_list.append("predicted_activity")
     formatted_score_terms = ["'Cached(%s)'" % term for term in additional_score_list]
     
@@ -20,22 +21,23 @@ def score_sdf(bcl_path,input_filename,model_dir,output_table,nprocs,additional_s
     
     flags = [
         "molecule:Properties",
-        "-input_filenames %s" % input_filename,
+        "-input_filenames %s" % " ".join(input_filenames),
         "-output_table %s" % output_path,
         "-add 'Mean(PredictedActivity(storage=File(directory=%s,prefix=model)))'" % model_dir,
         "-rename 'Mean(PredictedActivity(storage=File(directory=%s,prefix=model)))' predicted_activity" % model_dir,
         "-tabulate %s" % " ".join(formatted_score_terms),
     ]
     
-    if nprocs > 1:
-        flags.append("-scheduler PThread %d" % nprocs)
-    
     bcl_command += " " + " ".join(flags)
     
     subprocess.call(bcl_command, shell=True)
-
+    
+def chunks(l, n):
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
+        
 def init_options():
-    usage = "%prog --bcl_path=/path/to/bcl.exe --model_dir=ann_models input_file.sdf output_file.csv"
+    usage = "%prog --bcl_path=/path/to/bcl.exe --model_dir=ann_models input_file.sdf output_prefix"
     parser=OptionParser(usage)
     parser.add_option("-j",dest="nprocs",help="number of cpus to use",default=1)
     parser.add_option("--bcl_path",dest="bcl_path",help="path to bcl.exe",default="bcl.exe")
@@ -46,13 +48,25 @@ def init_options():
 if __name__ == "__main__":
     
     options,args = init_options().parse_args()
-    input_path = args[0]
-    output_path = args[1]
+    input_dir = args[0]
+    output_prefix = args[1]
     
     if options.other_scores != "":
         other_scores = options.other_scores.split(",")
     else:
         other_scores = []
     
-    score_sdf(options.bcl_path,input_path,options.model_dir,output_path,int(options.nprocs),other_scores)
+    input_paths = []
+    for path in glob.glob(input_dir+"/*"):
+        input_paths.append(path)
+    
+    jobs = []
+    for index,chunk in enumerate(chunks(input_paths,len(input_paths)/int(options.nprocs))):
+        jobs.append( (options.bcl_path,chunk,options.model_dir,output_prefix+"_"+str(index)+".csv",other_scores) )
+    
+    workers = Pool(int(options.nprocs))
+    workers.map(score_sdf,jobs)
+    workers.close()
+    workers.join()
+    #score_sdf(options.bcl_path,input_path,options.model_dir,output_path,other_scores)
     

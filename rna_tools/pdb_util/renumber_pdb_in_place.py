79d2e77e0b59909daebb0ed34f9beb1ba6461fa3
rhiju <rhiju@stanford.edu>
2013-12-19 10:29:33 -0800

    some coding utils and cleanup of make_rna_rosetta_ready & renumber_pdb_in_place.py

--- rna_tools/pdb_util/renumber_pdb_in_place.py
+++ rna_tools/pdb_util/renumber_pdb_in_place.py
@@ -4,16 +4,14 @@ from sys import argv
 from os import system
 from parse_options import parse_options, get_resnum_chain
 
-
 def renumber_pdb(pdbnames, new_numbers, chains, retain_atom_num = 0):
     for pdbname in pdbnames:
+
         lines = open(pdbname,'r').readlines()
 
         oldresnum = '   '
         count = 0;
-
         outid  = open( 'temp.txt','w')
-
         atomnum  = 0
         for line in lines:
             line_edit = line
@@ -23,7 +21,6 @@ def renumber_pdb(pdbnames, new_numbers, chains, retain_atom_num = 0):
             if line_edit[0:4] == 'ATOM' or line_edit[0:6] == 'HETATM':
 
                 if not (line[16]==' ' or line[16]=='A'): continue
-
                 atomnum += 1
                 oldchain = line_edit[21]
                 resnum = line_edit[23:26]
@@ -36,10 +33,10 @@ def renumber_pdb(pdbnames, new_numbers, chains, retain_atom_num = 0):
                     newchain = chains[ count - 1]
                     if newchain == '': newchain = line_edit[ 22 ]
                 else:
-                    if len( new_numbers) > 0: print 'WARNING! residue number %d is greater than length of input numbering %d' % (count, len( new_numbers) )
+                    if len( new_numbers) > 0:
+                        print 'WARNING! residue number %d is greater than length of input numbering %d' % (count, len( new_numbers) )
                     newnum = '%4d' % count
                     newchain = oldchain
-
                 if retain_atom_num:
                     line_edit = '%s%s%s' % (line_edit[0:22], newnum, line_edit[26:] )
                 else:
@@ -64,7 +61,6 @@ if __name__ == '__main__':
     new_numbers = []
     chains = []
     for i in range(1, len( argv ) ):
-        if not get_resnum_chain( argv[i], new_numbers, chains ):
+        if argv[i].find( '.pdb' )>0 or not get_resnum_chain( argv[i], new_numbers, chains ):
             pdbnames.append( argv[i] )
-
     renumber_pdb(pdbnames, new_numbers, chains, retain_atom_num)

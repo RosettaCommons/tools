7f153cbb14e5c320e98e9e677b22949697495ff9
rhiju <rhiju@stanford.edu>
2013-12-28 16:19:02 -0800

    saving scripts used for various SWM and SWA renaming initiatives

---
+++ coding_util/fixes/fix_AUTO_REMOVE.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+from sys import argv
+from os.path import abspath
+
+for file in argv[1:]:
+    if ( file.find( '.cc' ) == -1 and file.find( '.hh' ) == -1 ): print 'Skipping because not .cc file: ', file
+    lines = open( file ).readlines()
+    changed_a_line = False
+
+    new_lines = []
+    for line in lines:
+        if line.find( 'AUTO-REMOVE' ) > -1:
+            changed_a_line = True
+        else:
+            new_lines.append( line )
+
+    if changed_a_line:
+        print 'Changed file: ' , file
+        fid = open( file, 'w' )
+        for line in new_lines: fid.write( line )
+        fid.close()
+        continue
+

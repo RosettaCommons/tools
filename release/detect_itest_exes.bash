17cfffd84a4a0e96f451e2ed6c17f243295a41cf
Steven Lewis <smlewi@gmail.com>
2014-02-24 17:41:40 -0500

    ADDED BREAK to detect_itest_exes loop to not try to remove things twice.  Also whitespace change exposed by having to decide which loop to break.

--- release/detect_itest_exes.bash
+++ release/detect_itest_exes.bash
@@ -10,32 +10,33 @@ source ./../../../tools/release/release_common_functions.bash
 for directory in `ls tests`
 do
 
-#route around this one - it does bad things to our monster grep below (exposed by set -e; otherwise it just gets deleted and it's fine)
-if ["$directory" = "app_exception_handling"];
+    #route around this one - it does bad things to our monster grep below (exposed by set -e; otherwise it just gets deleted and it's fine)
+    if [ "$directory" == "app_exception_handling" ]
     then
-    continue
-fi
+	continue
+    fi
 
 #this monster finds all of the lines containing executable names from the tests' command files (first grep), strips the line down to only the part where the name is plus the flanking bin and binext string replacement junk (egrep), then also removes the string replacement junk (sed commands), then sorts and uniqs the list
-for executable in `grep "%(bin)s" tests/$directory/command | grep -v MY_MINI_PROGRAM | egrep -o '%\(bin\)s/.*%\(binext\)s' | sed 's/%(bin)s\///g' | sed 's/.%(binext)s//g' | sort | uniq`
-do
+    for executable in `grep "%(bin)s" tests/$directory/command | grep -v MY_MINI_PROGRAM | egrep -o '%\(bin\)s/.*%\(binext\)s' | sed 's/%(bin)s\///g' | sed 's/.%(binext)s//g' | sort | uniq`
+    do
 #echo $directory $executable
 
-    if grep -q $executable ../../source/src/apps.src.settings
+	if grep -q $executable ../../source/src/apps.src.settings
 	then
 	#if the executable is in the released build system, it's probably okay
-	echo $executable "found in apps.src.settings" $directory "may be ok"
+	    echo $executable "found in apps.src.settings" $directory "may be ok"
 	else
 	#if the executable is not found in the released build system, delete the test (as it won't run anyway)
-	echo $executable "not found, REMOVE" $directory
-	git rm -r tests/$directory #--dry-run
-	if [ "$debug" = false ];
+	    echo $executable "not found, REMOVE" $directory
+	    git rm -r tests/$directory #--dry-run
+	    if [ "$debug" = false ];
 	    then
-	    rm -r ref/$directory
+		rm -r ref/$directory
 	    else
-	    echo "DEBUG MODE ACTIVATED: skipping filesystem ref deletion of autoremoved integration tests"
-	fi
-    fi
-done
+		echo "DEBUG MODE ACTIVATED: skipping filesystem ref deletion of autoremoved integration tests"
+	    fi #end debug switch
+	    break #We've already removed this directory, skip any remaining entries for the directory
+	fi #If the executable was not found
+    done #for each executable in the directory's command file
 
-done
+done #for each command file

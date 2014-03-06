d99f021e94fe675de688ce2ecf26071d04dd1796
Rebecca Alford <rfalford12@gmail.com>
2014-03-06 00:57:47 -0500

    Adding some missing mpframework unit tests to mp ut utility script

--- membrane_tools/mptest_ut.py
+++ membrane_tools/mptest_ut.py
@@ -22,6 +22,7 @@ suites = [ 'EmbedDefIOTests',
 			'MembraneResiduesTest',
 			'MembraneResiduesTest',
 			'EmbeddingFactoryTest',
+			'MPDatabaseIOTest',
 			'MembraneConformationTest',
 			'MembraneProteinFactoryTest' ]
 

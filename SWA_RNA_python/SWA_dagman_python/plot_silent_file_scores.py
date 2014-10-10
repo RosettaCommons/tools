41761931e173f084d8dbbf1c6071e8445f51ced2
calebgeniesse <calebgeniesse@gmail.com>
2014-10-10 15:18:02 -0700

    Fixes for plotting score terms from silent files easily in python.

---
+++ SWA_RNA_python/SWA_dagman_python/plot_silent_file_scores.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from matplotlib import pyplot as plt
+import argparse 
+
+parser = argparse.ArgumentParser(description='Plot scores terms from silent files.')
+parser.add_argument('silent_file', help='Name of the Silent File')
+parser.add_argument('x', help='X-axis variable', default='rms')
+parser.add_argument('y', help='Y-axis variable', default='score')
+args=parser.parse_args()
+
+silent_file=args.silent_file
+xvar=args.x
+yvar=args.y
+
+score_keys = [line for line in open(silent_file, 'r').readlines() if 'SCORE' in line and 'description' in line][0].split()
+score_lines = [line for line in open(silent_file, 'r').readlines() if 'SCORE' in line and 'description' not in line]
+
+xdata = [float(cols[score_keys.index(xvar)]) for cols in [line.split() for line in score_lines]]
+ydata = [float(cols[score_keys.index(yvar)]) for cols in [line.split() for line in score_lines]]
+
+#for s,r in zip(score_,rms_):
+#	print s,', ',r
+
+plot = plt.plot(xdata,ydata, marker='o', color='r', linestyle=' ')
+plt.xlabel(xvar)
+plt.ylabel(yvar)
+plt.title(xvar+' vs. '+yvar)
+plt.show()

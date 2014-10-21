7c789b5446d9e66f9fe7c39465e1929761618339
calebgeniesse <calebgeniesse@gmail.com>
2014-10-21 10:35:00 -0700

    Debugging SWA_sampling_post_process.py

--- SWA_RNA_python/SWA_dagman_python/SWA_DAG/SWA_sampling_post_process_util.py
+++ SWA_RNA_python/SWA_dagman_python/SWA_DAG/SWA_sampling_post_process_util.py
@@ -42,10 +42,13 @@ def delete_sampler_outfiles_and_folders(indir_prefix, delete_files):
 
 	if(delete_files):
 		outfolder=indir_prefix +'/'
-		if(exists(outfolder)==False): error_exit_with_message("outfolder (%s) doesn't exist!" %(outfolder))
+		#if(exists(outfolder)==False): error_exit_with_message("outfolder (%s) doesn't exist!" %(outfolder))
+		if( not exists(outfolder) ):
+			print "outfolder (%s) doesn't exist!" % (outfolder)
+			return False
 		print "rm -r %s" %(outfolder)
 		submit_subprocess("rm -r %s" %(outfolder))
-
+		return True
 
 #############################################################################
 def concatenate_sampler_silent_file(condor_submit_file, cat_outfile):

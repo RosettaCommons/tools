65334d742b2918c4691b3f84dabe0fae1abdee5f
calebgeniesse <calebgeniesse@gmail.com>
2014-10-22 16:23:13 -0700

    Modified dagman/DAG_continuous_util.py to catch warnings as errors.

--- SWA_RNA_python/SWA_dagman_python/dagman/DAG_continuous_util.py
+++ SWA_RNA_python/SWA_dagman_python/dagman/DAG_continuous_util.py
@@ -420,15 +420,18 @@ def ensure_previous_job_has_no_error(slave_dir): ####Check for errors of the pre
 		rosetta_errfile = rosetta_errfile[:-1] #remove the new line (\n) character
 
 		if( ( exists(rosetta_errfile) ) and ( master_line_counts(rosetta_errfile)!=0 ) ):
-			if not is_warning_not_error(rosetta_errfile):	
+			if is_valid_error(rosetta_errfile):	
 				print "NON-EMPTY rosetta_errfile (%s)!" %(rosetta_errfile)
 				print "Error slave_node=%s " %(slave_dir)
 				master_kill_all_slave_jobs_and_exit()
 
 ##############################################
 
-def is_warning_not_error(errfile):
-	if 'ERROR' not in errfile:
+def is_valid_error(errfile):
+	for line in errfile:
+		if 'HESSIN for (i,i):' in line:	continue	
+		if 'G for (i):' in line:		continue
+		if not len(line):				continue
 		return True
 	else: 
 		return False

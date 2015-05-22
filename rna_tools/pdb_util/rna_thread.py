38d772864f9b2a9c4053c6440d35334145bbea40
calebgeniesse <calebgeniesse@gmail.com>
2015-05-22 04:02:52 -0700

    write rna_thread.cc wrappers for sequence threading and mutation

---
+++ rna_tools/pdb_util/rna_thread.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python
+
+###############################################################################
+### imports
+###############################################################################
+import sys
+import subprocess as sp
+import os
+import os.path
+import shutil
+import glob
+import argparse
+
+
+###############################################################################
+### helper functions
+###############################################################################
+def safe_submit(command):
+    print command if isinstance(command, str) else ' '.join(command)
+    out, err = sp.Popen(
+        command,
+        shell=isinstance(command, str),
+        stdout=sp.PIPE,
+        stderr=sp.PIPE
+    ).communicate()
+    if err:
+        print '\n'.join([out, err])
+        return False
+    return out
+
+
+###############################################################################
+### main functions
+###############################################################################
+def rna_thread(options):
+    options = init_options(options)
+    rna_thread = ["rna_thread"]
+    rna_thread.append('-s %s' % options.in_file_s)
+    rna_thread.append('-o %s' % options.out_file_o)
+    if options.fasta:
+        rna_thread.append('-fasta %s' % options.in_file_fasta)
+    if options.seq:
+        rna_thread.append('-seq %s' % options.seq)
+    if options.seq_offset:
+        rna_thread.append('-seq_offset %s' % options.seq_offset)
+    if options.residue_type_set:
+        rna_thread.append('-in:file:residue_type_set %s' % options.residue_type_set)
+    if options.options:
+        rna_thread += ['-' + o for o in options.options.split('-') if len(o)]
+    out = safe_submit(rna_thread)
+    if options.verbose:
+        print out
+    if not os.path.exists(options.out_file_o):
+        return None
+    return options.out_file_o 
+
+
+###############################################################################
+### init functions
+###############################################################################
+def init_options(options):
+    if isinstance(options, list):
+        parser = init_options_parser()
+        options = parser.parse_args(args = options)
+    if options.in_file_s is None:
+        print "[WARNING] Must supply a template pdb (-s) for threading!"
+        sys.exit(1)
+    if not len(filter(None, [options.fasta, options.seq])):
+        print "[WARNING] Must specify either -fasta, -seq or -mutate"
+        sys.exit(1)
+    return options
+
+def init_options_parser():
+    parser = argparse.ArgumentParser(
+        description='Wrapper for rna_thread.cc',
+        add_help=False
+    )
+    parser.add_argument(
+        "-s","-in:file:s",
+        dest="in_file_s",
+        help="Template pose '-s'",
+        default=None
+    )
+    parser.add_argument(
+        "-o","-out:file:o",
+        dest="out_file_o",
+        help="Outfile",
+        default="threaded.pdb"
+    )
+    parser.add_argument(
+        "-fasta","-in:file:fasta",
+        dest="fasta",
+        help="FASTA file",
+        default=None
+    )
+    parser.add_argument( 
+        "-seq",
+        help="target sequence (can include dashes). Must specify either this or -fasta. Length of this sequence must exactly equal length of template pose specified by -s",
+        default=None
+    )
+    parser.add_argument(
+        "-seq_offset",
+        help="Integer to add to all residue numbers in output PDB",
+        default=None
+    )
+    parser.add_argument(
+        "-sequence_mask_file",
+        help="Output name for sequence mask file (not in use at the moment)",
+        default=None
+    )
+    parser.add_argument(
+        "-residue_type_set", '-in:file:residue_type_set',
+        dest="residue_type_set",
+        help="residue type set, used in rosetta",
+        default=None
+    )
+    parser.add_argument(
+        "-options",
+        help="Extra Rosetta options",
+        default=None
+    )
+    parser.add_argument(
+        "-v","--verbose",
+        help="Increase verbosity",
+        action='store_true'
+    )
+    return parser
+
+###############################################################################
+### main
+###############################################################################
+if __name__=='__main__':
+    
+    print rna_thread(sys.argv[1:])

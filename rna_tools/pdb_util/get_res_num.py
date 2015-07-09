7f4fcac6c065e691316b13ca12649e24628d5123
calebgeniesse <calebgeniesse@gmail.com>
2015-07-09 15:18:35 -0700

    accept '-csv' as option in get_res_num.py

--- rna_tools/pdb_util/get_res_num.py
+++ rna_tools/pdb_util/get_res_num.py
@@ -2,8 +2,13 @@
 
 from read_pdb import read_pdb
 from sys import argv
-from make_tag import make_tag_with_dashes
+from make_tag import make_tag_with_dashes, make_tag_with_dashes_and_commas
 
+
+csv = False
+if '-csv' in argv:
+    argv.pop(argv.index('-csv'))
+    csv = True
 
 pdbs = argv[1:]
 
@@ -11,5 +16,8 @@ for main_pdb in pdbs:
 
     [ coords_main, lines_main, sequence_main, chains, residues ] = read_pdb( main_pdb )
 
-    print make_tag_with_dashes( residues )
+    if csv:
+             print make_tag_with_dashes_and_commas( residues )
+    else:
+             print make_tag_with_dashes( residues )
 

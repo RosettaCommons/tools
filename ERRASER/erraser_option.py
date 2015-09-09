d0d2119a0b11d1b67e036cc4507a819f4efca4d8
calebgeniesse <calebgeniesse@gmail.com>
2015-09-09 12:28:34 -0700

    set ERRASER option '-fcc2012_new_torsional_potential' default to 'True'

--- ERRASER/erraser_option.py
+++ ERRASER/erraser_option.py
@@ -70,7 +70,7 @@ class erraser_option :
         #2015 fixes
         self.o2prime_legacy_mode = True
         self.use_2prime_OH_potential = False
-        self.fcc2012_new_torsional_potential = False
+        self.fcc2012_new_torsional_potential = True
 
     def read_cmdline_full( self, argv ) :
         #General options
@@ -131,7 +131,7 @@ class erraser_option :
         #2015 fixes
         self.o2prime_legacy_mode = parse_options( argv, "o2prime_legacy_mode", "True" )
         self.use_2prime_OH_potential = parse_options( argv, "use_2prime_OH_potential", "False" )
-        self.fcc2012_new_torsional_potential = parse_options( argv, "fcc2012_new_torsional_potential", "False" )
+        self.fcc2012_new_torsional_potential = parse_options( argv, "fcc2012_new_torsional_potential", "True" )
 
         self.finalize()
 
@@ -167,7 +167,7 @@ class erraser_option :
         self.o2prime_legacy_mode = parse_options( argv, "o2prime_legacy_mode", "True" )
         self.scoring_file = parse_options( argv, "scoring_file", "" )
         self.use_2prime_OH_potential = parse_options( argv, "use_2prime_OH_potential", "False" )
-        self.fcc2012_new_torsional_potential = parse_options( argv, "fcc2012_new_torsional_potential", "False" )
+        self.fcc2012_new_torsional_potential = parse_options( argv, "fcc2012_new_torsional_potential", "True" )
 
         self.finalize()
 
@@ -201,7 +201,7 @@ class erraser_option :
         self.o2prime_legacy_mode = parse_options( argv, "o2prime_legacy_mode", "True" )
         self.scoring_file = parse_options( argv, "scoring_file", "" )
         self.use_2prime_OH_potential = parse_options( argv, "use_2prime_OH_potential", "False" )
-        self.fcc2012_new_torsional_potential = parse_options( argv, "fcc2012_new_torsional_potential", "False" )
+        self.fcc2012_new_torsional_potential = parse_options( argv, "fcc2012_new_torsional_potential", "True" )
 
         self.finalize()
 

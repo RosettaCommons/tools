691209407d06a4a3d0e425a9fa4761a5bc2f4fcc
Andrew Leaver-Fay <aleaverfay@gmail.com>
2015-09-15 15:59:50 -0400

    Moving Luki's auto-serialization script over from his branch (in the main repo; luki-mbi/serialization_prep) and editing the script for launching the clang-ast tool so that users no longer will have to edit the script for their machines.

--- clang_ast_transform/extract_serialization_data.sh
+++ clang_ast_transform/extract_serialization_data.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-# Run on all headers: python run_on_all_headers_w_fork.py -e $PWD/extract_serialization_data.sh -n 4
-# Then cat all .def files into one file, and pass it to make_serialize_templates.py to insert serialization stubs
+# To run on this script on all headers in the Rosetta source tree, use the following command from within
+# the Rosetta/main/source/src directory:
+#
+# python /path/to/tools/clang_ast_transform/run_on_all_headers_w_fork.py -e /path/to/tools/clang_ast_transform/extract_serialization_data.sh -n 4
+#
+# Once this has completed, cat all .def files into one file, and pass it to
+# make_serialize_templates.py to insert serialization stubs
 
-CLANG_BIN=/local/luki/clang/build/bin
-SOURCE=/local/luki/main/source
+# 1: assume that this is being run from somewhere within the rosetta source tree
+# 2: assume that this script lives in the Rosetta/tools/clang_ast_transform directory.
+#    The rosetta-refactor-tool must live in the
+#    Rosetta/tools/clang_ast_transform/clang/build/bin
+#    directory
+
+# get the directory where this script lives
+CLANG_AST_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+CLANG_BIN=$CLANG_AST_DIR/clang/build/bin
+
+# get the directory where we're executing this script
+SOURCE=$( pwd | sed 's:/src/: :' | awk '{print $1}' )
+
+# the file to "compile" should be the one and only argument to this script.
 FILE=$1
 
-cd $SOURCE
 $CLANG_BIN/rosetta-refactor-tool -matchers=find_record_decl,find_constructor_decl,find_field_decl $FILE -- \
 	clang++ -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS \
 	-std=c++11 \

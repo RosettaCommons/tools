b96dec4f62575b452e4f1c66798950f77ca88284
Steven Lewis <smlewi@gmail.com>
2016-05-21 20:18:36 -0400

    add examples for other nmbers

--- JD0/setup.bash
+++ JD0/setup.bash
@@ -4,13 +4,22 @@ cwd=`pwd`
 mpth=$cwd/subdirs
 echo $mpth
 
+#split this file into 5 files, don't break lines, use numeric suffixes padded to 1 digit
+#split -a 1 -n l/5 --numeric-suffixes=1 rosetta_commands.list allpdbs_slice_
+
 #split this file into 10 files, don't break lines, use numeric suffixes padded to 2 digits
 split -a 2 -n l/10 --numeric-suffixes=1 rosetta_commands.list allpdbs_slice_
+
+#split this file into 100 files, don't break lines, use numeric suffixes padded to 3 digits
+#split -a 3 -n l/100 --numeric-suffixes=1 rosetta_commands.list allpdbs_slice_
 
 rm -r $mpth
 mkdir $mpth
 
+#for subdir in `seq -w 5`
 for subdir in `seq -w 10`
+#for subdir in `seq -w 100`
+
 do
     echo $subdir
     mkdir $mpth/$subdir
--- JD0/submitter.bash
+++ JD0/submitter.bash
@@ -4,7 +4,10 @@ cwd=`pwd`
 mpth=$cwd/subdirs
 echo $mpth
 
+#for subdir in `seq -w 5`
 for subdir in `seq -w 10`
+#for subdir in `seq -w 100`
+
 do
     echo $subdir
     cd $mpth/$subdir

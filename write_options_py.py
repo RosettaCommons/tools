842fa7df19f02840ae5fa3947838b1615a0ed89f
Steven Lewis <smlewi@gmail.com>
2016-08-12 10:27:37 -0700

    gave up on original format, tweaking script to accelerate my life

--- write_options_py.py
+++ write_options_py.py
@@ -2,8 +2,8 @@
 import argparse
 
 print "This script takes a file containing options like so:\
-type name default description\
-Boolean PumpAction true use the PumpAction mover\
+type name description default\
+Boolean PumpAction use \"the PumpAction mover\" true\
 \
 Everything after the third item is parsed as the description\
 \
@@ -28,8 +28,8 @@ args = parser.parse_args()
 #args.input
 
 #expected line types
-#Boolean PumpAction true use the PumpAction mover
-#0        1         2    3:
+#Boolean PumpAction "use the PumpAction mover" true
+#0        1         2-x    last
 for line in open(args.input, 'r'):
     if line[0] == "#":
         pass
@@ -44,11 +44,11 @@ for line in open(args.input, 'r'):
     #process into named vars
     type = splitline[0]
     name = splitline[1]
-    default = splitline[2]
+    default = splitline[-1]
 
     #process multiple word desc
     desc = ""
-    for each in splitline[3:]:
+    for each in splitline[2:-1]:
         desc = desc + " "
     desc = desc.rstrip()
 
@@ -56,4 +56,3 @@ for line in open(args.input, 'r'):
     print "		Option('" + name + "', '" + type + "',"
     print "			default = '" + default + "',"
     print "			desc = '" + desc + "'"
-    

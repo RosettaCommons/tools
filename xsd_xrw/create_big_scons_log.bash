341886cde1707744b304e033ee9714de09718c12
Steven Lewis <smlewi@gmail.com>
2016-11-09 16:39:35 -0500

    minimal input validation

--- xsd_xrw/create_big_scons_log.bash
+++ xsd_xrw/create_big_scons_log.bash
@@ -3,7 +3,11 @@
 set -x #echo commands
 set -e #crash if any subcommand crashes
 
-echo "usage: create_big_scons_log.bash #procs_to_compile_at"
+if [ "$#" -ne 1 ]; then
+    echo "usage: create_big_scons_log.bash #procs_to_compile_at"
+    exit
+fi
+
 rm -rf build/*
 ./scons.py -j$1  > big_scons_log
 echo "big_scons_log should have your log file"
--- xsd_xrw/scons_one.bash
+++ xsd_xrw/scons_one.bash
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-echo "usage: scons_one.bash file_to_compile;  partial path is also acceptable"
+set -x #echo commands
+set -e #crash if any subcommand crashes
+
+if [ "$#" -ne 1 ]; then
+    echo "usage: scons_one.bash file_to_compile;  partial path is also acceptable"
+    exit
+fi
+
 echo "going to try this command:"
 grep big_scons_log $1
 grep big_scons_log $1 | source /dev/stdin

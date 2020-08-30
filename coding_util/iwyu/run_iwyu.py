123f94779a3d8a45256ee69bb8063023b19ac13c
Rocco Moretti <rmorettiase@gmail.com>
2020-08-30 16:36:25 -0500

    Initial, relatively decent status of IWYU based header removal.

---
+++ coding_util/iwyu/run_iwyu.py
@@ -0,0 +1,377 @@
+#!/usr/bin/env python
+
+'''run_iwyu.py - a script to run the clang tool "include-what-you-use" over
+the Rosetta codebase, and adjust the output for the vagarities of the Rosetta coding style.
+
+Run from the Rosetta/main/source directory and provide the script with files or directories,
+and the .cc .hh and .fwd.hh files therein will be processed.
+
+Because of order dependancy in removing headers, the default behavior is to create an *.iwyu
+file in the same location, listing what should be done to the file.
+'''
+
+from __future__ import print_function
+
+import sys, os
+import subprocess
+import codecs
+from fnmatch import fnmatch
+
+from optparse import OptionParser
+
+########## Internal config #############################
+
+#These are the clang commandline flags for debug mode, stripped of warning issues
+commandline_flags_linux = '''-c -std=c++11 -isystem external/boost_submod/ -isystem external/ -isystem external/include/ -isystem external/dbio/ -isystem external/libxml2/include -pipe -Qunused-arguments -DUNUSUAL_ALLOCATOR_DECLARATION -ftemplate-depth-256 -stdlib=libstdc++ -Wno-long-long -Wno-strict-aliasing -O0 -g -DBOOST_ERROR_CODE_HEADER_ONLY -DBOOST_SYSTEM_NO_DEPRECATED -DBOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS -DPTR_STD -Isrc -Iexternal/include -Isrc/platform/linux -ferror-limit=1'''.split()
+
+
+
+commandline_flags = commandline_flags_linux
+
+executable = 'include-what-you-use'
+
+DEBUG = False
+
+####################################
+
+SCRIPTDIR = os.path.dirname(os.path.realpath(__file__))
+
+NONSTANDARD_FORWARDS = {}
+if( os.path.exists(SCRIPTDIR+"/IWYU_nonstandard_fwd.txt") ):
+  with open(SCRIPTDIR+"/IWYU_nonstandard_fwd.txt") as f:
+    for line in f:
+        line = line.split()
+        if len(line) == 2 and line[0] != "#":
+            if line[0] in NONSTANDARD_FORWARDS:
+                print( "DUPLICATE ENTRY IN IWYU_nonstandard_fwd.txt!!!!!!! -", line[0] )
+            NONSTANDARD_FORWARDS[ line[0] ] = line[1]
+
+###################################
+
+def check_file_exists(filename):
+    '''We assume we're running in the main/source directory'''
+    return os.path.exists( 'src/' + filename )
+
+def process_namespace_line(line):
+    '''Convert a namespace line (like `namespace core { namespace chemical { class ResidueType; } }`) to forward header name'''
+    hierarchy = []
+    for entry in line.split():
+        if entry == 'namespace' or entry == '{' or entry == '}': continue
+        if entry == 'class' or entry == 'struct': continue
+        if entry == 'template' or entry == '<typename>': continue
+        if entry.endswith(';'):
+            hierarchy.append( entry[:-1] )
+        else:
+            hierarchy.append( entry )
+    return hierarchy
+
+def parse_current_includes(filename):
+    '''Get the current list of includes.
+
+    This is a little rough, as it doesn't consider issues with conditional compilation
+    '''
+    include_filenames = []
+    with open(filename) as f:
+        for line in f:
+            line = line.split()
+            if len(line) < 1 or not line[0] == '#include': continue
+            include_filenames.append( line[1][1:-1] ) # Split off the surrounding delimiters.
+
+    return include_filenames;
+
+class IWYUChanges:
+    def __init__(self, filename, additions, deletions, hh_adds=[]):
+        self.filename = filename
+        self.current_includes = parse_current_includes(filename)
+        self.additions = self.process_additions( additions ) # {file:[why]}
+        self.deletions = self.process_deletions( deletions ) # {file:[line]}
+        self.hh_additions = self.process_additions( hh_adds )
+        if DEBUG: self.prnt()
+        self.cleanup()
+
+    def process_deletions(self, deletions):
+        '''Deletions are somewhat simple, in that we just want the filename and the line number.'''
+        dels = {}
+        for line in deletions:
+            line = line.split()
+            filename = line[2][1:-1] # Assume semi-well formed.
+            lineno = line[-1].split('-')[0]
+            dels.setdefault( filename, [] ).append(lineno) # In case there's multiple lines for same.
+        return dels
+
+    def process_additions(self, additions):
+        adds = {}
+        for line in additions:
+            if line.startswith('#include'):
+                # Regular include
+                line = line.split()
+                filename = line[1][1:-1]
+                fors = line[4:]
+                adds.setdefault( filename, []).extend(fors)
+            elif line.startswith('namespace'):
+                hierarchy = process_namespace_line(line)
+                filename = '/'.join(hierarchy) + '.fwd.hh'
+                if filename in NONSTANDARD_FORWARDS:
+                    filename = NONSTANDARD_FORWARDS[filename]
+                if not check_file_exists(filename):
+                    print("ERROR: Can't find forward header file: ", filename)
+                else:
+                    adds.setdefault( filename, [ hierarchy[-1] ] )
+        return adds
+
+    def remove_deletion(self, fn):
+        if fn not in self.deletions: return
+        self.deletions[fn] = self.deletions[fn][1:] # Remove first
+        if len( self.deletions[fn] ) == 0:
+            del self.deletions[fn]
+
+    def remove_addition(self, fn):
+        if fn not in self.additions: return
+        del self.additions[fn]
+
+    def cleanup(self):
+        '''Do a bit of housekeeping.'''
+
+        # Don't delete our own header
+        if self.filename.endswith('.cc'):
+            parent_header = self.filename[:-3] + ".hh"
+        elif self.filename.endswith('.hh'):
+            parent_header = self.filename[:-3] + ".fwd.hh"
+        else:
+            parent_header = None
+        if parent_header in self.deletions:
+            if DEBUG: print("%% NO DELETE PARENT", parent_header)
+            self.remove_deletion( parent_header )
+
+        for fn in list( self.deletions.keys() ): # Copy as we're modifying structure in loop
+            # If we deleted from the cc file but added to hh file, don't delete
+            # The header should be processed separately.
+            if fn in self.hh_additions:
+                if DEBUG: print("%% NO DELETE ADDITION TO PARENT HEADER", fn)
+                self.remove_deletion( fn )
+                continue
+
+        for fn in list( self.additions.keys() ): # Copy as we're modifying structure in loop
+            #Don't delete then add.
+            if fn in self.deletions:
+                if DEBUG: print("%% NO ADD/DELETE", fn)
+                self.remove_deletion( fn )
+                self.remove_addition( fn )
+                continue
+            # Don't have both forward and regular
+            if fn.endswith('.fwd.hh'):
+                parent_fn = fn[:-7]+".hh"
+                # Don't add both
+                if parent_fn in self.additions:
+                    if DEBUG: print("%% NO ADD BOTH", fn)
+                    self.remove_addition( fn )
+                    continue
+                # Don't add if we already have the regular, and we're not deleting it
+                if parent_fn in self.current_includes and parent_fn not in self.deletions:
+                    if DEBUG: print("%% NO ADD, PARENT EXIST", fn)
+                    self.remove_addition( fn )
+                    continue
+            # Don't add if we already exist and we're not being deleted. (Mainly for fwds)
+            if fn in self.current_includes and fn not in self.deletions:
+                if DEBUG: print("%% NO ADD, ALREADY EXIST", fn)
+                self.remove_addition( fn )
+                continue
+
+
+    def prnt(self):
+        if len(self.deletions) == 0 and len(self.additions) == 0:
+            print("%%%%%%%%%% NO MOD NEEDED:", self.filename)
+        else:
+            print("%%%%%%%%%%%%%%%%%%%%%%%%%", self.filename)
+            print("~~~~~ Deleting")
+            print('\n'.join( fn + " // Line " + ','.join(lns) for fn, lns in self.deletions.items()))
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            print("~~~~~ Adding")
+            print('\n'.join( "#include <" + fn + "> // For " + ' '.join(whys) for fn, whys in self.additions.items()))
+            if DEBUG:
+                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                print("~~~~~ Current")
+                print('\n'.join( fn for fn in self.current_includes))
+            print("%%%%%%%%%%%%%%%%%%%%%%%%%", self.filename)
+
+
+def get_output_for_file(filename, options):
+    '''Get the raw iwyu output for the file, as lines'''
+    if not os.path.exists( SCRIPTDIR + '/boost-all.imp' ):
+        sys.stderr.write("Error: Please put IWYU's boost-all.imp file in the same directory as the script.\n")
+        sys.exit()
+
+    print( "%%% Processing ", filename )
+    command = [executable, '-Xiwyu', '--mapping_file=' + SCRIPTDIR + '/Rosetta.imp', '-Xiwyu', '--max_line_length=120']
+    for option in options.X:
+        command += [ '-Xiwyu', option ]
+
+    command += commandline_flags + [ filename ]
+
+    run = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    stdout, stderr = run.communicate()
+    stdout = codecs.decode( stdout, "UTF-8", "replace")
+    stderr = codecs.decode( stderr, "UTF-8", "replace")
+
+    if options.f:
+        sys.stdout.write( ' '.join(command) + "\n")
+        sys.stdout.write( stderr )
+        sys.stdout.write( "%%%\n" )
+
+    return stderr.split('\n')
+
+def print_parse_error( filename, output, message  ):
+    print( "###################################################" )
+    print( ">>>> ERROR with", filename, "::", message, "<<<<" )
+    sys.stdout.write('\n'.join(output)  )
+    print( ">>>> Skipping processing for", filename, "<<<<")
+    print( "###################################################")
+
+def parse_output( filename, output ):
+    hh_additions = []
+    additions = None
+    deletions = None
+    in_add_block = False
+    in_delete_block = False
+    in_hh_add_block = False
+
+    # We parse the block for hh lines in cc run because IWYU tends to move them up.
+    hh_filename = None
+    if filename.endswith('.cc'):
+        hh_filename = filename[:-3] + '.hh'
+
+    for line in output:
+        #Only trigger on the blocks for the file we're in.
+        if line.startswith(filename):
+            if 'should add these lines:' in line:
+                in_add_block = True
+                if additions is None:
+                    additions = []
+                continue
+            elif 'should remove these lines:' in line:
+                in_delete_block = True
+                if deletions is None:
+                    deletions = []
+                continue
+            # Oops, there's something off here
+            print_parse_error( filename, output, "Unexpected filename line" )
+            return None
+        if hh_filename is not None and line.startswith(hh_filename):
+            if 'should add these lines:' in line:
+                in_hh_add_block = True
+                continue
+            # We don't care if there's other blocks.
+        if line.strip() == "":
+            # end processing of block on empty line
+            in_add_block = False
+            in_delete_block = False
+            in_hh_add_block = False
+            continue
+        assert( not ( in_add_block and in_delete_block ) )
+        if in_add_block:
+            if not (line.startswith('#include') or line.startswith('namespace')):
+                print_parse_error( filename, output, "Addition block line doesn't start with include or namespace: " + line )
+            additions.append( line )
+            continue
+        if in_hh_add_block:
+            if not (line.startswith('#include') or line.startswith('namespace')):
+                print_parse_error( filename, output, "Addition block line doesn't start with include or namespace: " + line )
+            hh_additions.append( line )
+            continue
+        if in_delete_block:
+            if not line[0] == '-':
+                print_parse_error( filename, output, "Deletion block line doesn't start with minus: " + line )
+                return None
+            deletions.append( line )
+            continue
+
+
+    if additions is None or deletions is None:
+        print_parse_error( filename, output, "Could not find addition and/or deletions block" )
+        return None
+
+    return IWYUChanges(filename, additions, deletions, hh_additions)
+
+def check_file(filename, options):
+
+    output = get_output_for_file(filename, options)
+    mods = parse_output(filename, output)
+    if mods is not None and options.f:
+        mods.prnt()
+    return mods
+
+def process_file(filename, options):
+    if os.path.exists( filename + ".iwyu" ):
+        return
+
+    mods = check_file(filename, options)
+
+    mods.prnt()
+
+    return ###### !!!!!!
+
+
+    # Remove added includes if nested headers have them
+    # Should be in nested include order
+    for ii in range( len(mods) ):
+        for jj in range( ii+1, len(mods) ):
+            if not mods[ii].filename.startswith( mods[jj].filename[:-3] ):
+                print( "File "+mods[ii].filename+" is not in series with "+mods[jj].filename )
+                continue
+            mods[jj].nested_prune( mods[ii] )
+
+    for mod in mods:
+        mod.write_cache()
+        mod.prnt()
+
+def process_dir(dirname, options):
+    for item in os.listdir(dirname):
+        name = os.path.join(dirname,item)
+        if os.path.isdir(name):
+            process_dir(name, options)
+        elif os.path.isfile(name):
+            if name.endswith(".cc"):
+                #print( ">>> CC: ", name )
+                process_file(name, options)
+            elif name.endswith(".hh"):
+                # While info for the header is reported when processing the cc file,
+                # the info is different than when the hh file is processed itself,
+                # and doesn't look to be a "compile on your own" sense.
+                #print( ">>> HH: ", name )
+                process_file(name, options)
+
+if __name__ == "__main__":
+    if not os.path.exists( "./src" ):
+        print( "Script must be run from within Rosetta/main/source/" )
+        exit()
+
+    parser = OptionParser(usage="usage: %prog [OPTIONS] [FILES|DIRECTORIES]")
+    parser.set_description(__doc__)
+    parser.add_option("-f",
+      default=False, action="store_true",
+      help="Print the full output of include_what_you_use prior to processing it." )
+    parser.add_option("-I",
+      default=[], action="append",
+      help="Add the passed directories to the inclusion search path (good if the stdlib isn't being properly found." )
+    parser.add_option("-X",
+      default=[], action="append",
+      help="Add extra options to pass to Include What You Use." )
+    parser.add_option("--debug",
+      default=False, action="store_true",
+      help="Print extra debugging info." )
+
+    (options, args) = parser.parse_args(args=sys.argv[1:])
+
+    DEBUG = options.debug
+
+    if len(options.I) > 0:
+        commandline_flags += ['-I'+path for path in options.I]
+    for name in args:
+        if os.path.isdir(name):
+            process_dir(name, options)
+        elif os.path.isfile(name):
+            process_file(name, options)
+        else:
+            print("Cannot find file or directory: "+name)
+            exit()

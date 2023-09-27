009a1782a4d7bce57d3519b8c0fcb1e21fce0498
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2023-09-27 15:56:18 -0400

    Further updates for Python3.

--- fragment_tools/install_dependencies.pl
+++ fragment_tools/install_dependencies.pl
@@ -207,6 +207,7 @@ if ($overwrite || !-d "$Bin/sparks-x/bin" || !-d "$Bin/sparks-x/data") {
 	system( "patch sparks-x/SPINE-X/bin/buildinp_mat.pl < buildinp_mat.patch" ); 
 	system( "patch -R sparks-x/bin/buildinp.py < buildinp.patch" ); 
 	system( "patch -R sparks-x/bin/prolib.py < prolib.patch" ); 
+	system( "sed -i 's/ file(/ open(/g' sparks-x/bin/*.py" );
 	chdir("$Bin/sparks-x");
 	system("ln -sf ../blast ./");
 	system("ln -sf ../databases/ ./blast-NR");
